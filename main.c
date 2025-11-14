fcc1fe8dd5f909de6b23c60566ad1d5ca8a7f0f6
NavesDev <davinaves.2006@gmail.com>
2025-11-13 23:00:09 -0300

    Atualizado, começando a funcionar a lista

--- main.c
+++ main.c
@@ -1,28 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int ids_livros=0;
+// Nós para estrutura de lista s
 struct no{
     struct no* ultimo_no;
-    char* nome_livro;
-    char* nome_usuario;
+    char nome_livro[100];
+    char autor[80];
+    int id;
 };
 
-struct no* head;
+// Cabeça e cauda, para gerenciamento de listas
+struct no* head = NULL;
 
-void cadastrarEmprestimo(){
-    char nome_livro[50];
-    char nome_usuario[50];
+
+void cadastrarLivro(){
+    char nome_livro[100]; 
+    char autor[80];
     struct no* novo_no;
     novo_no = malloc(sizeof(struct no));
 
-    printf("%s","Digite o nome do livro a ser emprestado: ");
-    scanf("%s",nome_livro);
-    printf("%s","\nDigite o nome do usuário que irá pegar o livro emprestado: ");
-    scanf("%s",nome_usuario);
+    printf("%s","Digite o nome do livro a ser cadastrado: ");
+    scanf("%99s",nome_livro);
+    printf("%s","\nDigite o autor do livro a ser cadastrado: ");
+    scanf("%79s",autor);
 
+    novo_no->id = ids_livros++;
     novo_no->ultimo_no = head;
-    novo_no->nome_livro = nome_livro;
-    novo_no->nome_usuario = nome_usuario;
+
+    strcpy(novo_no->nome_livro, nome_livro);
+    strcpy(novo_no->autor, autor);
+
     head = novo_no;
+    printf("%s","\nCadastrado com sucesso!");
+}
+
+void buscarLivro(){
+    
+}
+
+// Constantes para a função ver livros
+const int TAMANHO_INICIAL = 32;
+
+void verLivros(){
+    if (head==NULL){
+        printf("%s","\nNenhum livro cadastrado!\n");
+        return;
+    }
+     
+    struct no* no_atual = head;
+    printf("%s","\n---/ Registro dos livros /---\n\n");
+    do{
+        printf("\nID: %i",no_atual->id);
+        printf("\nTítulo: %s",no_atual->nome_livro);
+        printf("\nAutor: %s",no_atual->autor);
+        printf("\n");
+        no_atual = no_atual->ultimo_no;
+    } while (no_atual->ultimo_no != NULL);
+
+
 }
 
 int main(){
@@ -30,15 +67,19 @@ int main(){
     while (1)
     {
         printf("%s", ":--- Registro de empréstimos ---: \n\n");
-        printf("%s","1. Consultar empréstimos\n2. Cadastrar empréstimos\n3. Sair\n");
+        printf("%s","1. Cadastrar livro\n2. Todos os livros\n3. Sair\n");
         scanf("%i",&escolha);
         if(escolha == 1){
-            cadastrarEmprestimo();
-        } else if (escolha == 2){
-
+            cadastrarLivro();            
+        } 
+        else if(escolha==2){
+            verLivros();
+        }
+        else if(escolha==3){
+            break;
         }
     }
-    
+
     
     return 0;
 } 
